4afe8fb94579d18755fd494e50b05bcafd5659a6
ReGeri <repasigeri@gmail.com>
2022-04-01 11:28:46 +0200

    Creating IGameModel interface

---
+++ Tank Combat/Models/IGameModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tank_Combat.Models
+{
+    internal interface IGameModel
+    {
+        event EventHandler Changed;
+        event EventHandler GameOver;
+
+        Tank PlayerTank { get; set; }
+        Tank EnemyTank { get; set; }
+        MapFrame MapFrame { get; set; }
+        List<Terrain> Terrains { get; set; }
+    }
+}
