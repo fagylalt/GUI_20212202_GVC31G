f4cc8091541018efdaa3e42e870ce24f69d11c78
danielbakos <bakidani0223@gmail.com>
2022-04-15 17:37:24 +0200

    Created menu structure

--- Tank Combat/MainWindow.xaml.cs
+++ Tank Combat/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tank_Combat.Menu.ViewModels;
+using Tank_Combat.Menu.Views;
 
 namespace Tank_Combat
 {
---
+++ Tank Combat/Menu/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Tank_Combat.Menu.Command;
+
+namespace Tank_Combat.Menu.ViewModels
+{
+    internal class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel selectedViewModel;
+        public MainViewModel()
+        {
+            this.UpdateViewCommand = new UpdateViewCommand(this);
+
+            this.ExitGameCommand = new RelayCommand(() => Environment.Exit(0));
+        }
+
+        public BaseViewModel SelectedViewModel
+        {
+            get
+            {
+                return this.selectedViewModel;
+            }
+
+            set
+            {
+                this.selectedViewModel = value;
+                this.OnPropertyChanged(nameof(this.SelectedViewModel));
+            }
+        }
+
+        public ICommand ExitGameCommand { get; set; }
+
+        public ICommand UpdateViewCommand { get; set; }
+    }
+}
