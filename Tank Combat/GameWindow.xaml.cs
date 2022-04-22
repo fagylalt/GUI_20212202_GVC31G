bb899a51382a7cd83ed99dad15fdb76cbe514eb9
Kálvin Ferenc Dániel <kalvinferenc@gmail.com>
2022-04-22 23:13:59 +0200

    Brushes and basic rendering finished

---
+++ Tank Combat/GameWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tank_Combat.Logic;
+
+namespace Tank_Combat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        TankCombatLogic logic;
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            logic = new TankCombatLogic((int)gameGrid.ActualWidth, (int)gameGrid.ActualHeight);
+            display.SetupModel(logic);
+            display.SizeSetup(new Size(gameGrid.ActualWidth, gameGrid.ActualHeight));
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            display.SizeSetup(new Size(gameGrid.ActualWidth, gameGrid.ActualHeight));
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
--- Tank Combat/Renderer/Display.cs
+++ Tank Combat/Renderer/Display.cs
@@ -7,17 +7,29 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using Tank_Combat.Models;
 
 namespace Tank_Combat.Renderer
 {
     internal class Display: FrameworkElement
     {
         Size area;
+        Random rand;
+        IGameModel model;
 
+        public Display()
+        {
+            rand = new Random();
+        }
         public void SizeSetup(Size _area)
         {
             this.area = _area;
             this.InvalidateVisual();
+        }
+        public void SetupModel(IGameModel _model)
+        {
+            this.model = _model;
+            this.model.Changed += (sender, EventArgs) => this.InvalidateVisual();
         }
         public Brush BackGroundBrush
         {
@@ -26,10 +38,85 @@ namespace Tank_Combat.Renderer
                 return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "Grassy_background.png"),UriKind.RelativeOrAbsolute)));
             }
         }
+
         protected override void OnRender(DrawingContext drawingContext)
         {
             base.OnRender(drawingContext);
-            drawingContext.DrawRectangle(BackGroundBrush, null, new Rect(0, 0, area.Width, area.Height));
+            if(area.Width> 0 && area.Height > 0 && model != null)
+            {
+                drawingContext.DrawRectangle(BackGroundBrush, null, new Rect(0, 0, area.Width, area.Height));
+                drawingContext.DrawRectangle(FriendlyTankBrush, null, new Rect(0,0,50,50));
+                drawingContext.DrawRectangle(EnemyBrush, null, new Rect(50,50, 50, 50));
+                drawingContext.DrawRectangle(BunkerBrush, null, new Rect(100, 100, 50, 50));
+                drawingContext.DrawRectangle(BuildingBrush, null, new Rect(150, 150, 50, 50));
+                drawingContext.DrawRectangle(WallBrush, null, new Rect(200,200, 50, 50));
+            }
+            
+            
         }
+        public Brush EnemyBrush
+        {
+            get
+            {
+                ImageBrush enemyImage = new ImageBrush();
+                int chosenEnemy = rand.Next(1, 4);
+                switch (chosenEnemy)
+                {
+                    case 1:
+                        enemyImage.ImageSource = new BitmapImage(new Uri(Path.Combine("Images", "RED_basic_tank.png"), UriKind.RelativeOrAbsolute));
+                        break;
+                    case 2:
+                        enemyImage.ImageSource = new BitmapImage(new Uri(Path.Combine("Images", "RED_heavy_tank.png"), UriKind.RelativeOrAbsolute));
+                        break;
+                    case 3:
+                        enemyImage.ImageSource = new BitmapImage(new Uri(Path.Combine("Images", "RED_light_tank.png"), UriKind.RelativeOrAbsolute));
+                        break;
+                }
+                return enemyImage;
+            }
+        }
+        public Brush FriendlyTankBrush
+        {
+            get
+            {
+                ImageBrush tankImage = new ImageBrush();
+                int chosenEnemy = rand.Next(1, 4);
+                switch (chosenEnemy)
+                {
+                    case 1:
+                        tankImage.ImageSource = new BitmapImage(new Uri(Path.Combine("Images", "BLUE_basic_tank.png"), UriKind.RelativeOrAbsolute));
+                        break;
+                    case 2:
+                        tankImage.ImageSource = new BitmapImage(new Uri(Path.Combine("Images", "BLUE_heavy_tank.png"), UriKind.RelativeOrAbsolute));
+                        break;
+                    case 3:
+                        tankImage.ImageSource = new BitmapImage(new Uri(Path.Combine("Images", "BLUE_light_tank.png"), UriKind.RelativeOrAbsolute));
+                        break;
+                }
+                return tankImage;
+            }
+        }
+        public Brush BunkerBrush
+        {
+            get 
+            {
+            return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "Bunker.png"), UriKind.RelativeOrAbsolute)));
+            }
+        }
+        public Brush BuildingBrush
+        {
+            get
+            {
+            return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "House.png"), UriKind.RelativeOrAbsolute)));
+            }
+        }
+        public Brush WallBrush
+        {
+            get
+            {
+            return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "Brick_wall.png"), UriKind.RelativeOrAbsolute)));
+            }
+        }
+
     }
 }
