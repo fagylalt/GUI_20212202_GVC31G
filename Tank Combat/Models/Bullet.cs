882e7c4043a025b3624e5833f46a3531458745c4
ReGeri <repasigeri@gmail.com>
2022-04-25 17:57:44 +0200

    Merge corrections

---
+++ Tank Combat/Models/Bullet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Tank_Combat.Models
+{
+    internal class Bullet : GameItem
+    {
+        #region Setup
+        public int CenterX { get; set; }
+        public int CenterY { get; set; }
+        public int SpeedX { get; set; }
+        public int SpeedY { get; set; }
+
+        public Bullet(int centerX, int centerY, int speedX, int speedY)
+        {
+            CenterX = centerX;
+            CenterY = centerY;
+            SpeedX = speedX;
+            SpeedY = speedY;
+        }
+
+        public override Geometry Area
+        {
+            get
+            {
+                return new EllipseGeometry(new Point(CenterX, CenterY), 10, 10);
+            }
+        }
+        #endregion
+
+        #region Methods
+        public void Move()
+        {
+            CenterX += SpeedX;
+            CenterY += SpeedY;
+        }
+        #endregion
+    }
+}
